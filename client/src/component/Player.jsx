0b50e01dd366dc47afcbdc6c78cf375de778a5ba
YasirHasn9 <hassanyasir223@gmail.com>
2019-10-13 18:30:17 -0700

    dark mode is working

--- client/src/component/Player.jsx
+++ client/src/component/Player.jsx
@@ -3,9 +3,8 @@ import {Grid , Paper} from "@material-ui/core"
 function Player({player}) {
     return (
         <div>
-            <Grid item xs={4}> 
+            <Grid item xs={2}> 
                 {player}
-               
                 </Grid>
         </div>
     )
